7f0ee7786fdf95f5b9d477409e2a72eb842690e4
romainniamor <romainnavoret@gmail.com>
2023-04-25 17:03:09 +0200

    pokemon generator with pixel style

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 const button = document.querySelector(".button");
-const pokeCard = document.querySelector(".pokeCard");
-const pokeBall = document.querySelector(".pokeball");
 const pokeName = document.querySelector(".pokeName");
 const pokeImg = document.querySelector(".pokeImg");
-const pokeType = document.querySelector(".pokeType");
 
 button.addEventListener("click", () => {
-  button.classList.add("animate"); // on ajoute au button et à la class pokeball animate qui déclence l'animation
-  pokeBall.classList.add("animate");
-
-  // on enleve aux elmts au bout de .4s
-  setTimeout(() => {
-    button.classList.remove("animate");
-    pokeBall.classList.remove("animate");
-  }, 400);
   changePokemon();
 });
 
@@ -28,24 +17,6 @@ const changePokemon = async () => {
 
   pokeImg.src = response.sprites.front_default; // on remplace la src de l elmt du dom par la response relative au sprite
   pokeName.textContent = `hehe! ... I'm ${response.name}!!!`; // text content remplacé par le nom
-  const type = response.types[0].type.name;
-  pokeType.textContent = type; // text content remplacé par la catégorie
-
-  if (type === "water" || type === "ice" || type === "flying") {
-    pokeType.style.backgroundColor = "#90dcfc";
-  } else if (
-    type === "fire" ||
-    type === "dragon" ||
-    type === "fighting" ||
-    type === "normal" ||
-    type === "electric"
-  ) {
-    pokeType.style.backgroundColor = "#ff6363";
-  } else if (type === "ground" || type === "rock" || type === "bug") {
-    pokeType.style.backgroundColor = "5a5c53";
-  } else if (type === "grass") {
-    pokeType.style.backgroundColor = "#4bf73b";
-  }
 };
 
 changePokemon(); // appel de la fuction
